0a831c835cc3ee47c63d42d4a4083ec93cb92f8a
lucasoliveirasouza <lycasoliveira@gmail.com>
2022-07-30 20:54:42 -0300

    Criação da interface

--- lib/views/counter.dart
+++ lib/views/counter.dart
@@ -5,6 +5,10 @@ class CounterView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
   }
 }
