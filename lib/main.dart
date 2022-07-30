2f04b88558b65968f6d68d59ccf1ec2efe2f5a74
lucasoliveirasouza <lycasoliveira@gmail.com>
2022-07-30 20:44:55 -0300

    Criação da interface

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:blocestudo/views/counter.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,11 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: "Counter",
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: CounterView(),
     );
   }
 }
---
+++ lib/views/counter.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CounterView extends StatelessWidget {
+  const CounterView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
