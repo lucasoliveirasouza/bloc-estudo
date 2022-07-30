59ec9ba1c63ffdabd99bdca2395ed593a86100f4
lucasoliveirasouza <lycasoliveira@gmail.com>
2022-07-30 20:58:56 -0300

    Criação da interface

--- lib/views/counter.dart
+++ lib/views/counter.dart
@@ -7,7 +7,26 @@ class CounterView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: [],
+        children: [
+          Container(
+            height: 250,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(50),
+              ),
+              color: Colors.blue.shade200,
+            ),
+            child: Center(
+              child: Text(
+                "Bloc Flutter",
+                style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
