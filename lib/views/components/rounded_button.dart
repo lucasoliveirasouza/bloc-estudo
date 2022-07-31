32703a4b427a244840ef569cab87e27c2e6524dc
lucasoliveirasouza <lycasoliveira@gmail.com>
2022-07-30 21:58:17 -0300

    Arrumando interface

---
+++ lib/views/components/rounded_button.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class RoundedButton extends StatelessWidget {
+  String title;
+  VoidCallback onTap;
+
+  RoundedButton({
+    Key? key,
+    required this.title,
+    required this.onTap,
+  }) : super(key: key);
+
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
