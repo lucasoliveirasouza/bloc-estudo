3d998994b077e5a61c334af361ff51b4a01deae7
lucasoliveirasouza <lycasoliveira@gmail.com>
2022-07-30 22:35:05 -0300

    Arrumando interface

--- lib/bloc/counter_book.dart
+++ lib/bloc/counter_book.dart
@@ -1,9 +1,27 @@
 import 'package:blocestudo/bloc/counter_event.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get/get.dart';
 
 class CounterBook extends Bloc<CounterEvent, int> {
   CounterBook() : super(0) {
+    on<decrement>((event, emit) {
+      if (state <= 0) {
+        Get.snackbar(
+          "Buying Books",
+          "Can not be less than zero",
+          icon: Icon(Icons.alarm),
+          barBlur: 20,
+          backgroundColor: Colors.redAccent.shade200,
+          isDismissible: true,
+          duration: Duration(
+            seconds: 3,
+          ),
+        );
+      } else {
+        return emit(state - 1);
+      }
+    });
     on<increment>((event, emit) => emit(state + 1));
-    on<decrement>((event, emit) => emit(state - 1));
   }
 }
--- lib/bloc/counter_pen.dart
+++ lib/bloc/counter_pen.dart
@@ -1,9 +1,27 @@
 import 'package:blocestudo/bloc/counter_event.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get/get.dart';
 
 class CounterPen extends Bloc<CounterEvent, int> {
   CounterPen() : super(0) {
     on<increment>((event, emit) => emit(state + 1));
-    on<decrement>((event, emit) => emit(state - 1));
+    on<decrement>((event, emit) {
+      if (state <= 0) {
+        Get.snackbar(
+          "Buying Pens",
+          "Can not be less than zero",
+          icon: Icon(Icons.alarm),
+          barBlur: 20,
+          backgroundColor: Colors.redAccent.shade200,
+          isDismissible: true,
+          duration: Duration(
+            seconds: 3,
+          ),
+        );
+      } else {
+        return emit(state - 1);
+      }
+    });
   }
 }
